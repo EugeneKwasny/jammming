faef44df9df828b5cd580c0ef0f195dde64cfeaa
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2022-09-10 12:39:31 +0300

    Section: Obtain a Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,54 @@
+const clientId =   '6900ff22f0dd4ba18e4e32de24709142';
+const redirectUri = 'http://localhost:3000/';
+
+let accessToken;
+
+const Spotify  = {
+
+    getAccessToken()
+    {
+        if(accessToken){
+            return accessToken;
+        }
+
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            //this clears thhe parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(()=>accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }else{
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    },
+
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).token(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }))
+        })
+    }
+}
+
+export default Spotify;
