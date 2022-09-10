0c8e7d675723a1b203097d8931c028fb02d19fed
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2022-09-10 14:59:53 +0300

    Fix auth headers

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -59,7 +59,7 @@ const Spotify  = {
         const headers = {Authorization: `Bearer ${accessToken}`};
         let userId;
 
-        return fetch('https://api.spotify.com/v1/me', {header: headers})
+        return fetch('https://api.spotify.com/v1/me', {headers: headers})
                 .then (response => response.json())
                 .then(jsonResponse => {
                     userId = jsonResponse.id;
